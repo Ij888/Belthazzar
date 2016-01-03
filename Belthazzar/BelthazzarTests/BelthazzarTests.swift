dbc62e9cfdff8cb2d1389fee0c9c72e0a6a82b68
Ijem <ij.ofili@gmail.com>
2016-01-03 23:45:53 +0000

    added entry

---
+++ Belthazzar/BelthazzarTests/BelthazzarTests.swift
@@ -0,0 +1,36 @@
+//
+//  BelthazzarTests.swift
+//  BelthazzarTests
+//
+//  Created by Ijem Ofili on 03/01/2016.
+//  Copyright © 2016 i0_Industries. All rights reserved.
+//
+
+import XCTest
+@testable import Belthazzar
+
+class BelthazzarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
